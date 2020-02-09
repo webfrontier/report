7d1b39a0ef460a7ce91ff9f475f645ce7841bd67
“nkudo” <kudo_27nov@yahoo.co.jp>
2020-02-10 07:24:48 +0900

    add timeline components

---
+++ src/components/Timeline.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {ListGroup, Badge} from 'react-bootstrap';
+import {timeline} from '../redux/actions';
+
+const mapStateToProps = (state) => {
+  return {reports: state.timeline}
+};
+
+const Timeline = ({reports, timeline}) => {
+  return (
+    <div className='page-body'>
+      <h3>最近投稿された月報</h3>
+      <ListGroup>
+        {
+          reports.map((report) =>
+            <ListGroup.Item action href="#">
+              {report.userName} {report.targetDate}分　
+              <small className="text-muted">投稿日: {report.date}  </small>
+              <div className="float-right"><Badge variant="success">{report.tag}</Badge></div>
+            </ListGroup.Item>
+          )
+        }
+      </ListGroup>
+      <a className="float-right" href="#">月報一覧はこちら</a>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, timeline)(Timeline);
