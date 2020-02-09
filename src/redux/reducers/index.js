41ec31cffceb92bfb6b302b80fc71473ae91543a
“nkudo” <kudo_27nov@yahoo.co.jp>
2020-02-10 07:28:57 +0900

    add notice and timeline of reducer list

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,5 +2,7 @@ import { combineReducers } from 'redux';
 import visibilityFilter from './visibilityFilter';
 import todos from './todos';
 import login from './login';
+import notice from './notice';
+import timeline from './timeline';
 
-export default combineReducers({ todos, visibilityFilter, login });
+export default combineReducers({ todos, visibilityFilter, login, notice , timeline});
