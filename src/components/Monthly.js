595c85042e24778ae9664744d6e41e7b73637a5a
“nkudo” <kudo_27nov@yahoo.co.jp>
2020-02-19 07:12:18 +0900

    add monthly components

---
+++ src/components/Monthly.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import MonthlySearch from './MonthlySearch';
+import Timeline from './Timeline';
+import {monthly} from '../redux/actions';
+
+const mapStateProps = (state) => {
+  return ({
+    active: state.active
+  });
+};
+
+const Monthly = (mapStateProps) => {
+  return (
+    <div>
+      <h1>月報トップ</h1>
+      <MonthlySearch/>
+      <Timeline/>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateProps,
+  monthly
+)(Monthly);
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -5,3 +5,5 @@ export const LOGIN = "LOGIN";
 export const NOTICE = "NOTICE";
 export const TIMELINE = "TIMELINE";
 export const MONTHLY_SEARCH = "MONTHLY_SEARCH";
+export const APP = "APP";
+export const MONTHLY = "MONTHLY";
